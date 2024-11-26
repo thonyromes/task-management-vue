6f997ef3d132fed0ffd467cbabecef6a44a171f0
Romes <thonyromes@gmail.com>
2024-11-26 16:25:50 +0100

    feat: setup tailwind prettier

---
+++ prettier.config.js
@@ -0,0 +1,9 @@
+/** @type {import('prettier').Config} */
+export default {
+  plugins: ["prettier-plugin-tailwindcss"],
+  semi: true,
+  tabWidth: 2,
+  singleQuote: false,
+  printWidth: 80,
+  trailingComma: "es5",
+};
