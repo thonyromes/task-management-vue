de0204a56c5acb47793a35f18d44688f97cff722
Romes <thonyromes@gmail.com>
2024-11-26 18:06:32 +0100

    fix: search input dropdown

--- src/components/TaskList.vue
+++ src/components/TaskList.vue
@@ -5,18 +5,20 @@
       <!-- Search -->
       <div class="md:col-span-6">
         <div class="form-control">
-          <div class="input-group">
+          <div class="relative">
             <input
               v-model="searchQuery"
               type="text"
               placeholder="Search tasks..."
-              class="input input-bordered w-full"
+              class="input input-bordered w-full pl-10"
               @input="handleSearch"
             />
-            <button class="btn btn-square btn-primary">
+            <div
+              class="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none"
+            >
               <svg
                 xmlns="http://www.w3.org/2000/svg"
-                class="h-5 w-5"
+                class="h-5 w-5 text-gray-400"
                 fill="none"
                 viewBox="0 0 24 24"
                 stroke="currentColor"
@@ -28,7 +30,7 @@
                   d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
                 />
               </svg>
-            </button>
+            </div>
           </div>
         </div>
       </div>
