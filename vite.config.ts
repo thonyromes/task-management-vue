7a6eda1e885aa074959c7705ece5824630f74b2a
Romes <thonyromes@gmail.com>
2024-11-26 23:08:40 +0100

    feat: update packages

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import vue from "@vitejs/plugin-vue";
+import vueJsx from "@vitejs/plugin-vue-jsx";
 import path from "path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
