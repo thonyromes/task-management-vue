7a7c182fc3addc87b93491eb8a488cabdc599ef2
Romes <thonyromes@gmail.com>
2024-11-26 23:13:36 +0100

    fix: build issues

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,10 @@ import vue from "@vitejs/plugin-vue";
 import vueJsx from "@vitejs/plugin-vue-jsx";
 import path from "path";
 import { defineConfig } from "vite";
+import vueDevTools from "vite-plugin-vue-devtools";
 
 export default defineConfig({
-  plugins: [vue(), vueJsx()],
+  plugins: [vue(), vueJsx(), vueDevTools()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
