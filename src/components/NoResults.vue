2995661660d55dea8f9588cf81f6885ba56c9706
Romes <thonyromes@gmail.com>
2024-11-26 20:09:09 +0100

    refactor: update the substask not found

--- src/components/NoResults.vue
+++ src/components/NoResults.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="alert alert-info shadow-lg m-4">
+  <div class="alert alert-info shadow-lg m-4 w-fit mx-auto">
     <svg
       xmlns="http://www.w3.org/2000/svg"
       fill="none"
--- src/components/SubtaskList.vue
+++ src/components/SubtaskList.vue
@@ -55,26 +55,12 @@
     </div>
 
     <!-- No Subtasks Message -->
-    <div v-else class="alert alert-info">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-        class="stroke-current shrink-0 w-6 h-6"
-      >
-        <path
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          stroke-width="2"
-          d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-        />
-      </svg>
-      <span>No subtasks yet. Add one above.</span>
-    </div>
+    <NoResults v-else message="No subtasks yet. Add one above." />
   </div>
 </template>
 
 <script setup lang="ts">
+import NoResults from "@/components/NoResults.vue";
 import type { Subtask } from "@/types/task";
 import { ref } from "vue";
 
